47c14549aba239cba436b98d727f511aa45e0fb9
Patrick Juen <patrickrainier@gmail.com>
2024-01-26 13:14:38 +0900

    provision vpc

--- terraform/core-infra/provider.tf
+++ terraform/core-infra/provider.tf
@@ -1,22 +1,15 @@
-locals {
-  project_id = "YOUR_PROJECT_ID"
-  region     = "YOUUR_REGION"
-  default_labels = {
-    managed-by = "terraform"
-  }
-}
-
 terraform {
-  required_version = "~> 1.5"
+  required_version = ">= 1.5"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.2"
+      version = ">= 5.2"
     }
   }
 
   backend "gcs" {
-    bucket = "YOUR_BUCKET"
+    bucket = "bcw-eb4c0a3f0bb6c5d0-tfstate"
+    prefix = "terraform/state/core-infra"
   }
 }
 
